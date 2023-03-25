1c2276c1631766fb880d409ecb061e6ced5d058c
Abbosbek-cloud <abek01sulaymonov@gmail.com>
2023-03-25 05:41:23 +0500

    88, 125 problems solved

---
+++ 100-days-coding-challange/day_4/index.js
@@ -0,0 +1,34 @@
+// LeetCode 88. Merge Sorted Array
+
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+
+var merge = function (nums1, m, nums2, n) {
+  let counter = 0;
+  for (let i = 0; i < nums1.length; i++) {
+    if (i >= m) {
+      nums1.splice(i, 1, nums2[counter]);
+      counter++;
+    }
+  }
+  nums1.sort((first, second) => first - second);
+  return nums1;
+};
+
+// LeetCode 125. Valid Palindrome
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function (s) {
+  let result = s.replace(/[^a-z0-9]/gi, "").toLowerCase();
+  return result.split("").reverse().join("") === result ? true : false;
+};
+
+console.log(isPalindrome("0P"));
