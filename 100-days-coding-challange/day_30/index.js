c10155969f0347ef945f3e5307afe8855b1a1fc2
Abbosbek-cloud <abek01sulaymonov@gmail.com>
2023-04-20 22:19:08 +0500

    day 30 completed

---
+++ 100-days-coding-challange/day_30/index.js
@@ -0,0 +1,16 @@
+// LeetCode 2626. Array Reduce Transformation
+
+/**
+ * @param {number[]} nums
+ * @param {Function} fn
+ * @param {number} init
+ * @return {number}
+ */
+
+var reduce = function (nums, fn, init) {
+  let answer = init;
+  for (let i = 0; i < nums.length; i++) {
+    answer = fn(answer, nums[i]);
+  }
+  return answer;
+};
