d4b8d667a503f5b219cd5daa9f15c7c61f1f7243
Abbosbek-cloud <abek01sulaymonov@gmail.com>
2023-04-27 21:31:38 +0500

    completion: 37th day

---
+++ 100-days-coding-challange/day_37/index.js
@@ -0,0 +1,23 @@
+// LeetCode 704. Binary Search
+
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+
+var search = function (nums, target) {
+  let left = 0;
+  right = nums.length;
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+    if (nums[mid] === target) {
+      return mid;
+    } else if (nums[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+  return -1;
+};
