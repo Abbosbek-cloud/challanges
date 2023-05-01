59fc3afab4ee808aefceb4b1c2271c7e9e3ae4ed
Abbosbek-cloud <abek01sulaymonov@gmail.com>
2023-05-01 22:51:49 +0500

    comletion: day 41

---
+++ 100-days-coding-challange/day_41/index.js
@@ -0,0 +1,14 @@
+var containsNearbyDuplicate = function (nums, k) {
+  const hashmap = new Map();
+  let index = 0,
+    len = nums.length;
+
+  while (index < len) {
+    if (index - hashmap.get(nums[index]) <= k) {
+      return true;
+    }
+    hashmap.set(nums[index], index);
+    index++;
+  }
+  return false;
+};
