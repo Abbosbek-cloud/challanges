3c19227a3f1b185e2d04a7d4d5940636a99d8a9e
Abbosbekm <abek01sulaymonov@gmail.com>
2023-05-23 10:50:49 +0500

    completion: day 63

---
+++ 100-days-coding-challange/day_63/index.js
@@ -0,0 +1,56 @@
+var MyHashMap = function () {
+  this.keys = [];
+  this.values = [];
+};
+
+/**
+ * @param {number} key
+ * @param {number} value
+ * @return {void}
+ */
+
+MyHashMap.prototype.put = function (key, value) {
+  // if key exists, replace it's value; else append;
+  let i = this.keys.indexOf(key);
+
+  if (i >= 0) {
+    this.values[i] = value;
+  } else {
+    this.keys.push(key);
+    this.values.push(value);
+  }
+};
+
+/**
+ * @param {number} key
+ * @return {number}
+ */
+
+MyHashMap.prototype.get = function (key) {
+  for (let i = 0; i < this.keys.length; i++) {
+    if (this.keys[i] === key) return this.values[i];
+  }
+  return -1;
+};
+
+/**
+ * @param {number} key
+ * @return {void}
+ */
+
+MyHashMap.prototype.remove = function (key) {
+  let i = this.keys.indexOf(key);
+
+  if (i >= 0) {
+    this.keys.splice(i, 1);
+    this.values.splice(i, 1);
+  }
+};
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * var obj = new MyHashMap()
+ * obj.put(key,value)
+ * var param_2 = obj.get(key)
+ * obj.remove(key)
+ */
