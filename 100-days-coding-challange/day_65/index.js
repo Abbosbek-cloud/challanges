694464db6299039ea62d4663a3eca90d489e3f4a
Abbosbekm <abek01sulaymonov@gmail.com>
2023-05-25 21:37:07 +0500

    completion: day 65

---
+++ 100-days-coding-challange/day_65/index.js
@@ -0,0 +1,14 @@
+// LeetCode 628. Maximum Product of Three Numbers
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+var maximumProduct = function (nums) {
+  nums.sort((a, b) => a - b);
+  return Math.max(
+    nums[nums.length - 1] * nums[nums.length - 2] * nums[nums.length - 3],
+    nums[nums.length - 1] * nums[0] * nums[1]
+  );
+};
