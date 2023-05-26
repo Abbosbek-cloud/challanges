6ec90dd81dad21d7f985ae5ced57b8bbd7b15c87
Abbosbekm <abek01sulaymonov@gmail.com>
2023-05-26 19:40:44 +0500

    completion: day 66

---
+++ 100-days-coding-challange/day_66/index.js
@@ -0,0 +1,20 @@
+// LeetCode 605. Can Place Flowers
+
+/**
+ * @param {number[]} flowerbed
+ * @param {number} n
+ * @return {boolean}
+ */
+var canPlaceFlowers = function (flowerbed, n) {
+  for (let i = 0; i < flowerbed.length && n !== 0; i++) {
+    if (
+      flowerbed[i] === 0 &&
+      flowerbed[i - 1] !== 1 &&
+      flowerbed[i + 1] !== 1
+    ) {
+      n--;
+      i++;
+    }
+  }
+  return n === 0;
+};
