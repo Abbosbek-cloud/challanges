22daa0a05330af7dd6d7ddca0cdb53b7d6a1b303
Abbosbekm <abek01sulaymonov@gmail.com>
2023-06-15 21:51:23 +0500

    completion: day 86

---
+++ 100-days-coding-challange/day_86/index.js
@@ -0,0 +1,51 @@
+// LeetCode 804. Unique Morse Code Words
+
+/**
+ * @param {string[]} words
+ * @return {number}
+ */
+
+var uniqueMorseRepresentations = function (words) {
+  var morse = [
+    ".-",
+    "-...",
+    "-.-.",
+    "-..",
+    ".",
+    "..-.",
+    "--.",
+    "....",
+    "..",
+    ".---",
+    "-.-",
+    ".-..",
+    "--",
+    "-.",
+    "---",
+    ".--.",
+    "--.-",
+    ".-.",
+    "...",
+    "-",
+    "..-",
+    "...-",
+    ".--",
+    "-..-",
+    "-.--",
+    "--..",
+  ];
+
+  var transformations = new Set();
+
+  for (let word of words) {
+    var trans = "";
+    for (let letter of word) {
+      var index = letter.charCodeAt(0) - 97;
+      trans += morse[index];
+    }
+
+    transformations.add(trans);
+  }
+
+  return transformations.size;
+};
